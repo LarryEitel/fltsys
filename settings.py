284e62438c2ceadd920fc0e14c746578040e3aba
Larry Eitel <larry@eitel.com>
2012-01-11 16:41:15 -0600

    static

--- settings.py
+++ settings.py
@@ -164,6 +164,7 @@ LOGGING = {
     }
 }
 
+
 # local_settings.py can be used to override environment-specific settings
 # like database and email that differ between development and production.
 try:
