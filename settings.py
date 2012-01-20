902f0842c255da72c0e2e06c8deb99248fa10f3b
Larry Eitel <larry@eitel.com>
2012-01-19 22:22:05 -0600

    try without staticfiles

--- settings.py
+++ settings.py
@@ -136,7 +136,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     'django.contrib.gis',
