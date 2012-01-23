a405cba571c044ab82f05d882b4734fe61f1a47e
www-data <www-data@xc.xchg.com>
2012-01-23 13:06:22 -0600

    removed some files

--- evernote/en_local_settings.py
+++
@@ -1,5 +0,0 @@
-# YOU MUST FILL THESE IN! GET ACCESS AT: http://www.evernote.com/about/developer/api/
-MY_CONSUMER_KEY = 'LarryEitel'
-MY_CONSUMER_SECRET = 'd4d29ab8758a1334'
-MY_USER = 'larry@eitel.com'
-MY_PW = 'zaq12wsx'
