2dc9593d8c9e9e83d23845c042e77b7af520215a
Larry Eitel <larry@eitel.com>
2012-01-29 09:55:06 -0600

    debugging js error

--- fabfile.py
+++ fabfile.py
@@ -56,7 +56,7 @@ def update_remote():
     
     with cd(fab['PROJECT_ROOT']): 
         run('git pull origin master') # pull from repository to remote 
-        #run('python manage.py collectstatic -v0 --noinput')
+        run('python manage.py collectstatic -v0 --noinput')
          
 def restart(): 
     sudo('supervisorctl restart flt')
